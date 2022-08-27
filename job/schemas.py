3b126c3d6af97c9fc64ad77749bad03d6b2d4c9c
Muhammad Hazim Al Farouq <muhammadhazim86@gmail.com>
2022-08-27 14:51:47 +0700

    moving image property to JobUpdate class

--- job/schemas.py
+++ job/schemas.py
@@ -41,6 +41,7 @@ class Job(BaseModel):
 
 class JobUpdate(Job):
     id: int = Field(None)
+    image: str = Field(None)
 
     class Config:
         orm_mode = True
@@ -49,7 +50,7 @@ class JobUpdate(Job):
 class JobInDB(JobUpdate):
     applicants: list[UserJobInDB] = Field([])
     creator: str = Field(None)
-    image: str = Field(None)
+
 
     class Config:
         orm_mode = True
