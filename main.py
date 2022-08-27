05ee54e87670b12f46810cdf1b7593f65c07e9a3
Muhammad Hazim Al Farouq <muhammadhazim86@gmail.com>
2022-08-27 15:27:36 +0700

    adding for CORS

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from userjob import controller as user_job_controller
 from sub_app2 import items
 import models
 from database import SessionLocal, engine
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 
@@ -15,3 +17,20 @@ app.include_router(auth_controller.router)
 app.include_router(job_controller.router)
 app.include_router(user_job_controller.router)
 ## app.include_router(items.router)
+
+
+origins = [
+    'https://carigawe-be.herokuapp.com',
+    'http://carigawe-be.herokuapp.com',
+    "http://localhost",
+    "http://localhost:8080",
+    'http://localhost:3000/'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
